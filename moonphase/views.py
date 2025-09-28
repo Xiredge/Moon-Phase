4905413f2254e3eeb3c2789b628746ad5478a402
Xiredge <jerick.gamba@outlook.ph>
2025-09-28 21:43:36 +0800

    Revamp UI and added todays moonphase upon first loadout

--- moonphase/views.py
+++ moonphase/views.py
@@ -1,12 +1,16 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 from .moon import get_moon_phase  # import your function
+import datetime
 
 def home(request):
     moon = None
     selected_date = None
 
     displays = ""
+
+    current_date = datetime.date.today()
+
 
     if 'date' in request.GET:
         selected_date = request.GET.get('date')  # format: 'YYYY-MM-DD'
@@ -16,11 +20,13 @@ def home(request):
             displays = "This webpage can only calculate the moon phases between January 1, 1900 and December 31, 2052"
         else:
             moon = get_moon_phase(year, month, day)
+    else:
+        moon = get_moon_phase(current_date.year, current_date.month, current_date.day)
+
 
 
     return render(request, 'home.html', {
         'moon': moon,
         'selected_date': selected_date,
-
         'displays': displays,
     })
